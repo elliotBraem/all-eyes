bdb8df5b3d75341750df060106d5b6a7fe368a95
elliotBraem <braemelliot@gmail.com>
2018-11-27 20:30:03 -0600

    feat: adds basic eye detection

--- all-eyes/face_detect.py
+++ all-eyes/face_detect.py
@@ -1,31 +1,43 @@
+import numpy as np
 import cv2
 import sys
 
-# Get user supplied values
-imagePath = sys.argv[1]
-cascPath = "haarcascade_frontalface_default.xml"
+# Path to the input image
+imgPath = 'abba.png'
 
-# Create the haar cascade
-faceCascade = cv2.CascadeClassifier(cascPath)
+# Paths to the XML classifiers
+faceCascPath = "resources/haarcascade_frontalface_default.xml"
+eyeCascPath = "resources/haarcascade_eye.xml"
 
-# Read the image
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# Load the required XML classifiers
+face_cascade = cv2.CascadeClassifier(faceCascPath)
+eye_cascade = cv2.CascadeClassifier(eyeCascPath)
+
+# Load the image
+img = cv2.imread(imgPath)
+# Convert to grayscale
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Detect faces in the image
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30),
-    flags = cv2.CASCADE_SCALE_IMAGE
+faces = face_cascade.detectMultiScale(
+    img_gray,
+    scaleFactor=1.3,
+    minNeighbors=5
 )
+# For each face found (x,y are coordinates, w,h are width and height)
+for (x,y,w,h) in faces:
+    # Draw a rectangle around the face
+    img = cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+    # Create a ROI (region of interest) for the face
+    roi_gray = img_gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    # Detect the eyes on the face
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    # For each eye found
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0,255,0), 2)
 
-print("Found {0} faces!".format(len(faces)))
-
-# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-cv2.imshow("Faces found", image)
+# Show results
+cv2.imshow("Face & Eye Detection Results", img)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
